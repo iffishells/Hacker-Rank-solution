b0f0475fef78e86d69b50e4ad1b53eac3b41a3c6
iffishells <iffishells@gmail.com>
2021-10-13 13:38:44 +0500

    Solved the problem

--- 2D Array - DS/HourGlassProblem.py
+++ 2D Array - DS/HourGlassProblem.py
@@ -4,16 +4,18 @@ import numpy as np
 
 
 def Split_in_3_by_3_Matrix(matrix):
-    glass_sum = 0
+    glass_sum = -1
     for i in range(0,4):
 
         for j in range(0,4):
-            greater_sum = matrix[i][j] + matrix[i][j+1] +matrix[i][j+2]+matrix[i+1][j+1]+matrix[i+2][j] + matrix[i+2][j+1] +matrix[i+2][j+2]           
+            greater_sum = (matrix[i][j]) + (matrix[i][j+1]) + (matrix[i][j+2])+ (matrix[i+1][j+1])+ (matrix[i+2][j]) + (matrix[i+2][j+1]) + (matrix[i+2][j+2])           
 
             print(matrix[i][j],",", matrix[i][j+1] ,",",matrix[i][j+2],",",matrix[i+1][j+1],",",matrix[i+2][j] , ",",matrix[i+2][j+1],",",matrix[i+2][j+2] )
-
+            # print("Greater sum Sum  ^ : ",greater_sum)
+            print("Greater Sum :",greater_sum ,  " ","glass_sum :" ,glass_sum)
             if greater_sum > glass_sum:
                 glass_sum = greater_sum
+                print("Glass_sum  :",glass_sum)
 
     return glass_sum          
 
@@ -54,7 +56,8 @@ def SumhourGlass_3_X_3_Matrix(arr):
 # Array  = np.arange(36).reshape(6,6) # splitting problem in 3 by 3 matrix
 
 # test Case
-Array = [1, 1, 1, 0, 0, 0,0, 1, 0, 0, 0, 0,1, 1 ,1 ,0 ,0, 0,0, 0 ,2 ,4, 4, 0,0, 0 ,0 ,2 ,0 ,0,0 ,0 ,1 ,2 ,4 ,0]
+# Array = [1, 1, 1, 0, 0, 0,0, 1, 0, 0, 0, 0,1, 1 ,1 ,0 ,0, 0,0, 0 ,2 ,4, 4, 0,0, 0 ,0 ,2 ,0 ,0,0 ,0 ,1 ,2 ,4 ,0]
+Array = [ -1 ,-1 ,0 ,-9, -2, -2,-2 ,-1 ,-6, -8, -2,-5,-1 ,-1 ,-1 ,-2 ,-3, -4,-1, -9, -2, -4, -4, -5 ,-7, -3, -3, -2, -9, -9,-1 ,-3 ,-1, -2, -4,-5]
 Array = np.array(Array).reshape(6,6)
 print("Input Array\n",Array)
 print("\n\n\n")
